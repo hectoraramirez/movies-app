a6ee39bf4396dcf57b3ef0be6ca5ba6522137246
hectoraramirez <hector.leon.83@gmail.com>
2020-09-20 17:29:17 -0500

    Update production environment
    set url of api-movies in the file environment prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  urlDiscover: 'https://api.themoviedb.org/3/discover/movie?api_key=b38bb8d99f6fcd4f5f044b473a25a3f1',
+  urlMovie: 'https://api.themoviedb.org/3/movie/',
+  api_key: '?api_key=b38bb8d99f6fcd4f5f044b473a25a3f1'
 };
